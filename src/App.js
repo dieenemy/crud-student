71dabc5fcf15e130850635fbd51bb2b1f26f0d7c
Tran Xuan Tung <tungtx@hybrid-technologies.vn>
2021-10-13 10:02:29 +0700

    create folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Classes from './containers/Classes/Classes';
+import Students from './containers/Students/Students';
+import Subjects from './containers/Subjects/Subjects';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Header />
+     <Switch>
+       <Route exact path="/" component={Students} />
+       <Route  path="/classes" component={Classes} />
+       <Route  path="/subjects" component={Subjects} />
+     </Switch>
+     <Footer />
     </div>
   );
 }
